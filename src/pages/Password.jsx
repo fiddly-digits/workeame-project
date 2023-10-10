2c4aaf92f52a30a354fc55ab1e3b95d315b405ba
sam <samantha.castillo.g@gmail.com>
2023-10-09 23:24:59 -0600

    fix:adjust inputs

--- src/pages/Password.jsx
+++ src/pages/Password.jsx
@@ -1,11 +1,11 @@
-import { Input, Button } from '@nextui-org/react';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { EyeAlt, EyeClose } from 'iconoir-react';
+import { Input, Button } from "@nextui-org/react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { EyeAlt, EyeClose } from "iconoir-react";
 
 // TODO: Password Recovery on Login Page
 
@@ -14,42 +14,42 @@ const schema = Yup.object().shape({
     !value
       ? Yup.string()
       : Yup.string()
-          .required('La contraseña es requerida')
-          .min(8, 'Debe contener al menos 8 caracteres')
-          .matches(/[A-Z]/, 'Debe contener al menos una mayúscula')
-          .matches(/[0-9]/, 'Debe contener al menos un número')
+          .required("La contraseña es requerida")
+          .min(8, "Debe contener al menos 8 caracteres")
+          .matches(/[A-Z]/, "Debe contener al menos una mayúscula")
+          .matches(/[0-9]/, "Debe contener al menos un número")
           .matches(
             /[!@#$%^&*()_+{}[\]:;<>,.?~\\-]/,
-            'Debe contener al menos un cáracter especial'
+            "Debe contener al menos un cáracter especial"
           )
   ),
   newPassword: Yup.lazy((value) =>
     !value
       ? Yup.string()
       : Yup.string()
-          .required('La contraseña es requerida')
-          .min(8, 'Debe contener al menos 8 caracteres')
-          .matches(/[A-Z]/, 'Debe contener al menos una mayúscula')
-          .matches(/[0-9]/, 'Debe contener al menos un número')
+          .required("La contraseña es requerida")
+          .min(8, "Debe contener al menos 8 caracteres")
+          .matches(/[A-Z]/, "Debe contener al menos una mayúscula")
+          .matches(/[0-9]/, "Debe contener al menos un número")
           .matches(
             /[!@#$%^&*()_+{}[\]:;<>,.?~\\-]/,
-            'Debe contener al menos un cáracter especial'
+            "Debe contener al menos un cáracter especial"
           )
           .notOneOf(
-            [Yup.ref('oldPassword'), null],
-            'La contraseña nueva debe ser diferente a la actual'
+            [Yup.ref("oldPassword"), null],
+            "La contraseña nueva debe ser diferente a la actual"
           )
-  )
+  ),
 });
 
 export default function Password() {
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
-    mode: 'onBlur',
-    resolver: yupResolver(schema)
+    mode: "onBlur",
+    resolver: yupResolver(schema),
   });
   const navigate = useNavigate();
   const [isNewVisible, setIsNewVisible] = useState(false);
@@ -62,18 +62,18 @@ export default function Password() {
 
   async function onSubmit(data) {
     console.log(data);
-    const token = sessionStorage.getItem('token');
+    const token = sessionStorage.getItem("token");
     axios
-      .patch('http://localhost:8080/api/v1/user/passwordChange/', data, {
+      .patch("http://localhost:8080/api/v1/user/passwordChange/", data, {
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        }
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
       })
       .then((res) => {
         console.log(res);
-        sessionStorage.removeItem('token');
-        navigate('/login', { replace: true });
+        sessionStorage.removeItem("token");
+        navigate("/login", { replace: true });
         window.location.reload();
       })
       .catch((err) => {
@@ -81,32 +81,32 @@ export default function Password() {
       });
   }
   return (
-    <div className='flex flex-col m-10'>
-      <h2 className='w-full mb-8 text-2xl text-slate-500 font-oswald'>
+    <div className="flex flex-col m-10">
+      <h2 className="w-full mb-8 text-2xl text-black font-oswald">
         Modifica tu Password
       </h2>
       <form
-        className='flex flex-col items-center gap-3 border'
+        className="flex flex-col items-center gap-5"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <p className='text-slate-500 font-roboto'>
-          {' '}
+        <p className="text-gray-500 font-roboto">
+          {" "}
           Al modificar tu Password tendras que volver a ingresar con el nuevo
           password.
         </p>
         <Input
-          type={isOldVisible ? 'text' : 'password'}
-          label='Contraseña Actual'
-          size='sm'
-          className='font-roboto'
+          type={isOldVisible ? "text" : "password"}
+          label="Contraseña Actual"
+          size="sm"
+          className="font-roboto"
           isRequired
-          variant='bordered'
+          variant="bordered"
           errorMessage={errors.oldPassword?.message}
           isInvalid={!!errors.oldPassword}
           endContent={
             <button
-              className='focus:outline-none'
-              type='button'
+              className="focus:outline-none"
+              type="button"
               onClick={toggleOldVisibility}
             >
               {isOldVisible ? (
@@ -116,21 +116,21 @@ export default function Password() {
               )}
             </button>
           }
-          {...register('oldPassword')}
+          {...register("oldPassword")}
         />
         <Input
-          type={isNewVisible ? 'text' : 'password'}
-          label='Nueva Contraseña'
-          size='sm'
-          className='font-roboto'
+          type={isNewVisible ? "text" : "password"}
+          label="Nueva Contraseña"
+          size="sm"
+          className="font-roboto"
           isRequired
-          variant='bordered'
+          variant="bordered"
           errorMessage={errors.newPassword?.message}
           isInvalid={!!errors.newPassword}
           endContent={
             <button
-              className='focus:outline-none'
-              type='button'
+              className="focus:outline-none"
+              type="button"
               onClick={toggleNewVisibility}
             >
               {isNewVisible ? (
@@ -140,22 +140,22 @@ export default function Password() {
               )}
             </button>
           }
-          {...register('newPassword')}
+          {...register("newPassword")}
         />
         {errorMessage && (
-          <div className='flex justify-center text-red-400 font-roboto'>
+          <div className="flex justify-center text-red-400 font-roboto">
             {errorMessage}
           </div>
         )}
         <Button
-          size='lg'
-          radius='md'
-          type='submit'
-          className='text-white bg-wkablack font-oswald hover:cursor-pointer'
+          size="lg"
+          radius="md"
+          type="submit"
+          className="text-white bg-wkablack font-oswald hover:cursor-pointer"
           {...(!errors.oldPassword && !errors.newPassword
             ? { isDisabled: false }
             : { isDisabled: true })}
-          startContent={<img src='/arrow-right.svg' alt='next' />}
+          startContent={<img src="/arrow-right.svg" alt="next" />}
         >
           CAMBIA
         </Button>
