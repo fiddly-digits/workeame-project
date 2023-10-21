09e9b50b67e430950830dd5e786ff18952ef46c2
sam <samantha.castillo.g@gmail.com>
2023-10-20 18:21:36 -0600

    fix:cards text-overflow

--- src/components/WorkerCardFlip.tsx
+++ src/components/WorkerCardFlip.tsx
@@ -1,5 +1,5 @@
-import { Card, CardBody, CardFooter } from '@nextui-org/card';
-import { Link } from 'react-router-dom';
+import { Card, CardBody, CardFooter } from "@nextui-org/card";
+import { Link } from "react-router-dom";
 
 interface Props {
   name: string;
@@ -18,29 +18,29 @@ export default function WorkerCardFlip({
   address,
   description,
   position,
-  route
+  route,
 }: Props) {
   return (
-    <div className='flip-card'>
-      <div className='flip-card-inner max-h-[28rem]'>
+    <div className="flip-card">
+      <div className="flip-card-inner max-h-[28rem]">
         <Card
-          shadow='sm'
+          shadow="sm"
           isPressable
           //onPress={() => }
           className={`m-2 flip-card-front rounded-${position}-[80px]`}
         >
-          <CardBody className='p-0 overflow-hidden shadow-sm radius-sm'>
+          <CardBody className="p-0 overflow-hidden shadow-sm radius-sm">
             <img
               alt={`${name}-${job}`}
-              className='object-cover w-full h-full'
+              className="object-cover w-full h-full"
               src={picture}
             />
           </CardBody>
-          <CardFooter className='flex-col font-roboto text-large'>
-            <b className='text-2xl font-bold uppercase tracking-[2px]'>
+          <CardFooter className="flex-col font-roboto text-large">
+            <b className="text-2xl font-bold uppercase tracking-[2px]">
               {name}
             </b>
-            <p className=' text-base text-gray-500 font-roboto font-extralight uppercase tracking-[1.5px]'>
+            <p className=" text-base text-gray-500 font-roboto font-extralight uppercase tracking-[1.5px]">
               {job}
             </p>
           </CardFooter>
@@ -50,41 +50,41 @@ export default function WorkerCardFlip({
           className={`flip-card-back rounded-${position}-[80px] flex justify-center`}
         >
           <Card
-            shadow='sm'
+            shadow="sm"
             isPressable
             //onPress={() => }
             className={`p-4 w-60 h-80 flip-card-front rounded-${position}-[80px]`}
           >
-            <CardBody className='p-0 overflow-visible shadow-sm radius-sm'>
-              <h2 className='text-3xl font-bold uppercase tracking-[3px] my-2'>
+            <CardBody className="p-0 overflow-visible shadow-sm radius-sm">
+              <h2 className="text-3xl font-bold uppercase tracking-[3px] my-2">
                 {name}
               </h2>
-              <h2 className='text-xl text-gray-500 font-roboto font-extralight uppercase tracking-[2px]'>
+              <h2 className="text-xl text-gray-500 font-roboto font-extralight uppercase tracking-[2px]">
                 {job}
               </h2>
-              <h3 className='text-[16px] font-roboto font-medium uppercase tracking-[4px] my-2'>
+              <h3 className="text-[16px] font-roboto font-medium uppercase tracking-[4px] my-2">
                 {address}
               </h3>
-              <p className='my-5 font-light text-justify text-gray-600 font-roboto'>
+              <p className="my-5 font-light text-justify line-clamp-6 text-ellipsis text-gray-600 font-roboto">
                 {description}
               </p>
             </CardBody>
-            <CardFooter className='flex-col font-roboto text-md'>
+            <CardFooter className="flex-col font-roboto text-md">
               <Link
                 to={`/ms/${route}`}
-                className='flex items-center gap-2 bottom-buttons-container'
+                className="flex items-center gap-2 bottom-buttons-container"
               >
-                {' '}
-                <p className='font-bold tracking-wider text-start font-roboto'>
-                  {' '}
+                {" "}
+                <p className="font-bold tracking-wider text-start font-roboto">
+                  {" "}
                   Conecta con {name}
                 </p>
                 <img
-                  src='/arrow.svg'
+                  src="/arrow.svg"
                   width={30}
                   height={30}
-                  alt='logo'
-                  className='-rotate-90 swing'
+                  alt="logo"
+                  className="-rotate-90 swing"
                 />
               </Link>
             </CardFooter>
